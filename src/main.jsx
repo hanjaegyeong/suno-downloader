b9dd4317e137e9748880229b4269f0b8d8a31be1
hanjaegyeong <hjg000223@naver.com>
2026-03-02 21:30:13 +0900

    feat: 언어별 타이틀/설명 변경, 에드센스 태그 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { getLang } from './i18n';
+import { getLang, setLang } from './i18n';
 import './styles.css';
 
-document.documentElement.lang = getLang();
+setLang(getLang());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
